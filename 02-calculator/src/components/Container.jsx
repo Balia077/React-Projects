1c81f69e4918513ecaea55bc9c8240ee51a02308
Balia077 <balaramd894@gmail.com>
2025-12-02 17:08:49 +0530

    Making container for the calculator

---
+++ 02-calculator/src/components/Container.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Container = () => {
+  return (
+    <div className='flex h-[600px] w-[500px] bg-gray-700 rounded-3xl'>
+      
+    </div>
+  )
+}
+
+export default Container
--- 02-calculator/src/components/Page.jsx
+++ 02-calculator/src/components/Page.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Container from './Container'
 
 const Page = () => {
   return (
-    <div className='text-white h-screen w-full bg-gray-900'>
-      
+    <div className='flex items-center justify-center text-white h-screen w-full bg-gray-900'>
+      <Container />
     </div>
   )
 }
